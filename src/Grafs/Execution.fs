8cf29fd50dc11ff21bc72c18d4794c009825745d
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2017-10-28 21:15:23 +0200

    native JSON support + Sigil dependency

--- src/Grafs/Execution.fs
+++ src/Grafs/Execution.fs
@@ -4,6 +4,7 @@
 namespace Grafs
 
 open System
+open System.IO
 open System.Collections.Generic
 open Grafs.Ast
 open Grafs.Types
@@ -12,129 +13,71 @@ open Grafs.Planning
 
 type Error = string * string list
 
-type GQLData =
-    | QObj of Map<string, GQLData>
-    | QList of GQLData list
-    | QNull
-    | QInt of int
-    | QFloat of double
-    | QBool of bool
+type GQLValue =
+    | GQLObj of struct (string * GQLValue) list
+    | GQLList of GQLValue list
+    | GQLNull
+    | GQLInt of int
+    | GQLFloat of double
+    | GQLBool of bool
+    | GQLString of string
 
-type Output = GQLData
+type Output = GQLValue
 type GQLResponse =
     | Direct of data:Output * errors: Error list
     | Deferred of data:Output * errors:Error list * defer:IObservable<Output>
     | Stream of stream:IObservable<Output>
     
-/// Name value lookup used as output to be serialized into JSON.
-/// It has a form of a dictionary with fixed set of keys. Values under keys
-/// can be set, but no new entry can be added or removed, once lookup
-/// has been initialized.
-/// This dicitionay implements structural equality.
-//type NameValueLookup(keyValues: KeyValuePair<string, obj> []) =
-//    let kvals = keyValues |> Array.distinctBy (fun kv -> kv.Key)
-//    let setValue key value =
-//        let mutable i = 0
-//        while i < kvals.Length do
-//            if kvals.[i].Key = key then
-//                kvals.[i] <- KeyValuePair<string, obj>(key, value) 
-//                i <- Int32.MaxValue
-//            else i <- i+1
-//    let getValue key = (kvals |> Array.find (fun kv -> kv.Key = key)).Value
-//    let rec structEq (x: NameValueLookup) (y: NameValueLookup) =
-//        if Object.ReferenceEquals(x, y) then true
-//        elif Object.ReferenceEquals(y, null) then false
-//        elif x.Count <> y.Count then false
-//        else
-//            x.Buffer
-//            |> Array.forall2 (fun (a: KeyValuePair<string, obj>) (b: KeyValuePair<string, obj>) ->
-//                if a.Key <> b.Key then false
-//                else 
-//                    match a.Value, b.Value with
-//                    | :? NameValueLookup, :? NameValueLookup as o -> structEq (downcast fst o) (downcast snd o)
-//                    | :? seq<obj>, :? seq<obj> -> Seq.forall2 (=) (a.Value :?> seq<obj>) (b.Value :?> seq<obj>)
-//                    | a1, b1 -> a1 = b1) y.Buffer
-//    let pad (sb: System.Text.StringBuilder) times = for i in 0..times do sb.Append("\t") |> ignore
-//    let rec stringify (sb: System.Text.StringBuilder) deep (o:obj) =
-//        match o with
-//        | :? NameValueLookup as lookup ->
-//            sb.Append("{ ") |> ignore
-//            lookup.Buffer
-//            |> Array.iter (fun kv -> 
-//                sb.Append(kv.Key).Append(": ") |> ignore
-//                stringify sb (deep+1) kv.Value
-//                sb.Append(",\r\n") |> ignore
-//                pad sb deep)
-//            sb.Remove(sb.Length - 4 - deep, 4 + deep).Append(" }") |> ignore
-//        | :? string as s ->
-//            sb.Append("\"").Append(s).Append("\"") |> ignore
-//        | :? System.Collections.IEnumerable as s -> 
-//            sb.Append("[") |> ignore
-//            for i in s do 
-//                stringify sb (deep+1) i
-//                sb.Append(", ") |> ignore
-//            sb.Append("]") |> ignore
-//        | other -> 
-//            if other <> null 
-//            then sb.Append(other.ToString()) |> ignore
-//            else sb.Append("null") |> ignore
-//        ()
-//    /// Create new NameValueLookup from given list of key-value tuples.
-//    static member ofList (l: (string * obj) list) = NameValueLookup(l)
-//    /// Returns raw content of the current lookup.
-//    member private x.Buffer : KeyValuePair<string, obj> [] = kvals
-//    /// Return a number of entries stored in current lookup. It's fixed size.
-//    member x.Count = kvals.Length
-//    /// Updates an entry's value under given key. It will throw an exception
-//    /// if provided key cannot be found in provided lookup.
-//    member x.Update key value = setValue key value
-//    override x.Equals(other) = 
-//        match other with
-//        | :? NameValueLookup as lookup -> structEq x lookup
-//        | _ -> false
-//    override x.GetHashCode() =
-//        let mutable hash = 0
-//        for kv in kvals do
-//            hash <- (hash*397) ^^^ (kv.Key.GetHashCode()) ^^^ (if kv.Value = null then 0 else kv.Value.GetHashCode())
-//        hash
-//    override x.ToString() = 
-//        let sb =Text.StringBuilder()
-//        stringify sb 1 x
-//        sb.ToString()
-//    interface IEquatable<NameValueLookup> with
-//        member x.Equals(other) = structEq x other
-//    interface System.Collections.IEnumerable with
-//        member x.GetEnumerator() = (kvals :> System.Collections.IEnumerable).GetEnumerator()
-//    interface IEnumerable<KeyValuePair<string, obj>> with
-//        member x.GetEnumerator() = (kvals :> IEnumerable<KeyValuePair<string, obj>>).GetEnumerator()
-//    interface IDictionary<string, obj> with
-//        member x.Add(_, _) = raise (NotSupportedException "NameValueLookup doesn't allow to add/remove entries")
-//        member x.Add(_) = raise (NotSupportedException "NameValueLookup doesn't allow to add/remove entries")
-//        member x.Clear() = raise (NotSupportedException "NameValueLookup doesn't allow to add/remove entries")
-//        member x.Contains(item) = kvals |> Array.exists ((=) item)
-//        member x.ContainsKey(key) = kvals |> Array.exists (fun kv -> kv.Key = key)
-//        member x.CopyTo(array, arrayIndex) = kvals.CopyTo(array, arrayIndex)
-//        member x.Count = x.Count
-//        member x.IsReadOnly = true
-//        member x.Item
-//            with get (key) = getValue key
-//            and set (key) v = setValue key v
-//        member x.Keys = upcast (kvals |> Array.map (fun kv -> kv.Key))
-//        member x.Values = upcast (kvals |> Array.map (fun kv -> kv.Value))
-//        member x.Remove(_:string) = 
-//            raise (NotSupportedException "NameValueLookup doesn't allow to add/remove entries")
-//            false
-//        member x.Remove(_:KeyValuePair<string,obj>) = 
-//            raise (NotSupportedException "NameValueLookup doesn't allow to add/remove entries")
-//            false
-//        member x.TryGetValue(key, value) = 
-//            match kvals |> Array.tryFind (fun kv -> kv.Key = key) with
-//            | Some kv -> value <- kv.Value; true
-//            | None -> value <- null; false
-//    new(t: (string * obj) list) = 
-//        NameValueLookup(t |> List.map (fun (k, v) -> KeyValuePair<string,obj>(k, v)) |> List.toArray)
-//    new(t: string []) = 
-//        NameValueLookup(t |> Array.map (fun k -> KeyValuePair<string,obj>(k, null)))
+module Json =
+
+    let rec toJson (writer: TextWriter) = 
+        function
+        | GQLNull     -> writer.Write("null")
+        | GQLInt n    -> writer.Write(n)
+        | GQLFloat n  -> writer.Write(n)
+        | GQLBool b   -> writer.Write(if b then "true" else "false")
+        | GQLString s -> 
+            writer.Write('"')
+            writer.Write(s)
+            writer.Write('"')
+        | GQLList l   -> 
+            let rec write' (writer: TextWriter)=
+                function
+                | [] -> ()
+                | [h] -> toJson writer h
+                | h::t  -> 
+                    toJson writer h
+                    writer.Write(',')
+                    write' writer t
+            writer.Write('[')
+            write' writer l
+            writer.Write(']')
+        | GQLObj o -> 
+            let rec write' (writer: TextWriter)=
+                function
+                | [] -> ()
+                | [struct (k: string,v)] ->
+                    writer.Write('"')
+                    writer.Write(k)
+                    writer.Write("\":")                    
+                    toJson writer v
+                | struct (k,v)::t  -> 
+                    writer.Write('"')
+                    writer.Write(k)
+                    writer.Write("\":")                    
+                    toJson writer v
+                    writer.Write(',')
+                    write' writer t
+            writer.Write('{')
+            write' writer o
+            writer.Write('}')  
+
+    type GQLValue with
+        member x.ToJson (writer: TextWriter) = toJson writer x
+        member x.ToJson () =
+            use writer = new StringWriter()
+            toJson writer x
+            writer.ToString()
                 
 module internal Execution =
 
---
+++ tests/Grafs.Tests/JsonTests.fs
@@ -0,0 +1,29 @@
+﻿/// The MIT License (MIT)
+/// Copyright (c) 2016 Bazinga Technologies Inc
+
+module Grafs.Tests.JsonTests
+
+open Expecto
+open Grafs
+open Grafs.Json
+
+
+let tests = 
+    testList "GQLValue" [
+        test "Can serialize itself to JSON string" {
+            let value = GQLObj [
+                struct ("a", GQLNull)
+                struct ("b", GQLInt 1)
+                struct ("c", GQLString "ok")
+                struct ("d", GQLBool true)
+                struct ("e", GQLBool false)
+                struct ("f", GQLFloat 1.2)
+                struct ("g", GQLList [ GQLString "ok"; GQLInt 1 ])
+                struct ("h", GQLObj [
+                    struct ("ha", GQLList [])
+                ])
+            ]
+            let expected = """{"a":null,"b":1,"c":"ok","d":true,"e":false,"f":1.2,"g":["ok",1],"h":{"ha":[]}}"""
+            Expect.equal expected (value.ToJson()) "GQLValue should be stringified to compact JSON string"
+        }
+    ]
